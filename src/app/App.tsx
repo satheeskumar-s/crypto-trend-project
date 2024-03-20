bb65cf616ae019d7be27bb84f2b722afe146ab35
satheeskumar-s <satheeskumar.s@eyepax.com>
2024-03-20 12:06:22 +0530

    Added mui to the app

---
+++ src/app/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import '../scss/app.scss';
+
+import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
+import Layout from "./components/layout/Layout";
+
+function App() {
+  const materialTheme = createTheme(/* your theme */);
+  const darkTheme = createTheme({
+    palette: {
+      mode: 'dark',
+    },
+  });
+  return (
+    <div className='App'>
+      <ThemeProvider theme={materialTheme}>
+        <CssBaseline />
+        <Layout />
+      </ThemeProvider>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/app/components/layout/Layout.tsx
@@ -0,0 +1,10 @@
+import NavBar from "../shared/NavBar";
+import React from "react";
+
+const Layout = () => {
+  return(
+      <NavBar />
+  )
+}
+
+export default Layout;
